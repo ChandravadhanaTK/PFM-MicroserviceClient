5a56fbd106bf48ec8cf86576d790fcbd6ca8422c
Munisai <MM5035249@SYNTELINC.COM>
2018-07-31 16:35:20 +0530

    Login module using Microservices

---
+++ pfm-login-ms/src/main/java/com/javaj2eefsd/workshop/dao/LoginDaoImpl.java
@@ -0,0 +1,39 @@
+package com.javaj2eefsd.workshop.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+import com.javaj2eefsd.workshop.api.ApiException;
+import com.javaj2eefsd.workshop.model.UserDetail;
+import com.javaj2eefsd.workshop.util.PFMConstants;
+
+@Repository
+public class LoginDaoImpl implements LoginDao {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Override
+    public UserDetail getLogin(final UserDetail userdetail) throws Exception {
+    	UserDetail loginuser = null;
+    	try {
+            final Query query = new Query();
+            query.addCriteria(
+                    Criteria.where("emailId").is(userdetail.getEmailId()).and("password").is(userdetail.getPassword()));
+            loginuser = mongoTemplate.findOne(query, UserDetail.class);
+        }
+        catch (final Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    	
+    	if (loginuser == null)
+            throw new ApiException(PFMConstants.ERROR_CODE, PFMConstants.INVALID_EMAILID_PASSWORD);
+        else if (loginuser.getUserStatus() == false)
+        	throw new ApiException(PFMConstants.ERROR_CODE, PFMConstants.USER_NOT_ACTIVE);
+
+        return userdetail;
+    }
+
+}
